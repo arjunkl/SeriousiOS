26267c21c5f57a134f78e356d5c63f393a2b55fc
arjunkl <58782223+arjunkl@users.noreply.github.com>
2026-07-17 02:06:06 +0530

    Route SDL OpenGL lookups through iOS compatibility adapters

---
+++ Sources/Platform/iOS/SeriousIOSSDLGLProcAddress.cpp
@@ -0,0 +1,21 @@
+#include "SeriousIOSPlatformBridge.h"
+
+#include <SDL.h>
+
+#include <dlfcn.h>
+
+extern "C" void* SDLCALL SDL_GL_GetProcAddress(const char* procedure) {
+    if (procedure == nullptr || *procedure == '\0') {
+        return nullptr;
+    }
+
+    if (void* textureProcedure =
+            SeriousIOS_GetOpenGLTextureCompatProcAddress(procedure)) {
+        return textureProcedure;
+    }
+    if (void* compatibilityProcedure =
+            SeriousIOS_GetOpenGLCompatProcAddress(procedure)) {
+        return compatibilityProcedure;
+    }
+    return dlsym(RTLD_DEFAULT, procedure);
+}
