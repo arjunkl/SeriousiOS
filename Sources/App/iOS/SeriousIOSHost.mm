a0fc2725bb6674edc4bfde9b57944125bec5e784
arjunkl <58782223+arjunkl@users.noreply.github.com>
2026-07-16 21:05:30 +0530

    Configure explicit iOS paths and EAGL context ownership

--- Sources/App/iOS/SeriousIOSHost.mm
+++ Sources/App/iOS/SeriousIOSHost.mm
@@ -9,16 +9,19 @@
 extern "C" bool SeriousIOS_TFE_RegisterEntitySymbols() noexcept;
 extern "C" bool SeriousIOS_TFE_RegisterRuntimeSymbols() noexcept;
 static NSString* const kEncounterName = @"The First Encounter";
+static NSString* const kEncounterPathComponent = @"TFE";
 #elif defined(SERIOUSIOS_TSE)
 extern "C" bool SeriousIOS_TSE_RegisterEntitySymbols() noexcept;
 extern "C" bool SeriousIOS_TSE_RegisterRuntimeSymbols() noexcept;
 static NSString* const kEncounterName = @"The Second Encounter";
+static NSString* const kEncounterPathComponent = @"TSE";
 #else
 #error Define SERIOUSIOS_TFE or SERIOUSIOS_TSE
 #endif
 
 @class SeriousIOSRenderView;
 static void SeriousIOSPresent(void* context);
+static int SeriousIOSMakeCurrent(void* context);
 
 namespace {
 
@@ -30,6 +33,63 @@ bool registerRuntimeSymbols() noexcept {
     return SeriousIOS_TSE_RegisterEntitySymbols()
         && SeriousIOS_TSE_RegisterRuntimeSymbols();
 #endif
+}
+
+NSURL* createEncounterDirectory(
+    NSFileManager* fileManager,
+    NSURL* root,
+    NSString* category,
+    NSError** error) {
+    NSURL* directory = [[[root URLByAppendingPathComponent:@"SeriousIOS" isDirectory:YES]
+        URLByAppendingPathComponent:kEncounterPathComponent isDirectory:YES]
+        URLByAppendingPathComponent:category isDirectory:YES];
+    if (![fileManager createDirectoryAtURL:directory
+               withIntermediateDirectories:YES
+                                attributes:nil
+                                     error:error]) {
+        return nil;
+    }
+    return directory;
+}
+
+bool configurePlatformPaths() {
+    NSFileManager* fileManager = NSFileManager.defaultManager;
+    NSError* error = nil;
+
+    NSURL* documentsRoot = [fileManager URLsForDirectory:NSDocumentDirectory
+                                                inDomains:NSUserDomainMask].firstObject;
+    NSURL* supportRoot = [fileManager URLsForDirectory:NSApplicationSupportDirectory
+                                              inDomains:NSUserDomainMask].firstObject;
+    NSURL* cacheRoot = [fileManager URLsForDirectory:NSCachesDirectory
+                                            inDomains:NSUserDomainMask].firstObject;
+    NSURL* temporaryRoot = [NSURL fileURLWithPath:NSTemporaryDirectory() isDirectory:YES];
+
+    NSURL* dataDirectory = createEncounterDirectory(
+        fileManager, documentsRoot, @"GameData", &error);
+    NSURL* userDirectory = createEncounterDirectory(
+        fileManager, supportRoot, @"User", &error);
+    NSURL* cacheDirectory = createEncounterDirectory(
+        fileManager, cacheRoot, @"Cache", &error);
+    NSURL* temporaryDirectory = createEncounterDirectory(
+        fileManager, temporaryRoot, @"Temporary", &error);
+
+    NSString* executablePath = NSBundle.mainBundle.executablePath;
+    if (error != nil
+        || executablePath.length == 0
+        || dataDirectory == nil
+        || userDirectory == nil
+        || cacheDirectory == nil
+        || temporaryDirectory == nil) {
+        NSLog(@"SeriousiOS path configuration failed: %@", error);
+        return false;
+    }
+
+    return SeriousIOS_ConfigurePaths(
+        executablePath.fileSystemRepresentation,
+        dataDirectory.path.fileSystemRepresentation,
+        userDirectory.path.fileSystemRepresentation,
+        cacheDirectory.path.fileSystemRepresentation,
+        temporaryDirectory.path.fileSystemRepresentation);
 }
 
 } // namespace
@@ -71,11 +131,13 @@ bool registerRuntimeSymbols() noexcept {
     _context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
     NSAssert(_context != nil, @"Unable to create an OpenGL ES 2 context");
     SeriousIOS_SetPresentCallback(&SeriousIOSPresent, (__bridge void*)self);
+    SeriousIOS_SetGLContext((__bridge void*)_context, &SeriousIOSMakeCurrent);
     return self;
 }
 
 - (void)dealloc {
     SeriousIOS_SetPresentCallback(nullptr, nullptr);
+    SeriousIOS_SetGLContext(nullptr, nullptr);
     [self destroyDrawable];
     if (EAGLContext.currentContext == _context) {
         [EAGLContext setCurrentContext:nil];
@@ -167,6 +229,11 @@ static void SeriousIOSPresent(void* context) {
     [view presentDrawable];
 }
 
+static int SeriousIOSMakeCurrent(void* context) {
+    EAGLContext* eaglContext = (__bridge EAGLContext*)context;
+    return [EAGLContext setCurrentContext:eaglContext] ? 0 : -1;
+}
+
 @interface SeriousIOSViewController : UIViewController
 @end
 
@@ -201,6 +268,9 @@ static void SeriousIOSPresent(void* context) {
     (void)application;
     (void)launchOptions;
 
+    if (!configurePlatformPaths()) {
+        return NO;
+    }
     if (!registerRuntimeSymbols()) {
         NSLog(@"SeriousiOS static runtime registration failed for %@", kEncounterName);
         return NO;
