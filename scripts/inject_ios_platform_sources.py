e6f2419657d749026ce729056004de054e9e3422
arjunkl <58782223+arjunkl@users.noreply.github.com>
2026-07-18 16:28:35 +0530

    Build real iOS audio output and bundled Vorbis

--- scripts/inject_ios_platform_sources.py
+++ scripts/inject_ios_platform_sources.py
@@ -28,10 +28,13 @@ if(IOS)
         message(FATAL_ERROR "SERIOUSIOS_PLATFORM_ROOT must point to the SeriousiOS platform source directory")
     endif()
     include_directories("${SERIOUSIOS_PLATFORM_ROOT}")
+    include_directories("${CMAKE_SOURCE_DIR}/libogg/include")
+    include_directories("${CMAKE_SOURCE_DIR}/libvorbis/include")
+    include_directories("${CMAKE_SOURCE_DIR}/libvorbis/lib")
     set_source_files_properties(
         "${SERIOUSIOS_PLATFORM_ROOT}/SeriousIOSSDLCompat.cpp"
         PROPERTIES COMPILE_DEFINITIONS
-            "SDL_GL_GetProcAddress=SeriousIOS_OriginalSDL_GL_GetProcAddress;SDL_GetMouseState=SeriousIOS_OriginalSDL_GetMouseState;SDL_GetKeyboardState=SeriousIOS_OriginalSDL_GetKeyboardState;SDL_GetScancodeFromKey=SeriousIOS_OriginalSDL_GetScancodeFromKey;SDL_GetRelativeMouseState=SeriousIOS_OriginalSDL_GetRelativeMouseState;SDL_PollEvent=SeriousIOS_OriginalSDL_PollEvent;SDL_NumJoysticks=SeriousIOS_OriginalSDL_NumJoysticks;SDL_JoystickNameForIndex=SeriousIOS_OriginalSDL_JoystickNameForIndex;SDL_JoystickOpen=SeriousIOS_OriginalSDL_JoystickOpen;SDL_JoystickClose=SeriousIOS_OriginalSDL_JoystickClose;SDL_JoystickNumAxes=SeriousIOS_OriginalSDL_JoystickNumAxes;SDL_JoystickNumButtons=SeriousIOS_OriginalSDL_JoystickNumButtons;SDL_JoystickNumHats=SeriousIOS_OriginalSDL_JoystickNumHats;SDL_JoystickInstanceID=SeriousIOS_OriginalSDL_JoystickInstanceID;SDL_JoystickGetAxis=SeriousIOS_OriginalSDL_JoystickGetAxis;SDL_JoystickGetButton=SeriousIOS_OriginalSDL_JoystickGetButton")
+            "SDL_GL_GetProcAddress=SeriousIOS_OriginalSDL_GL_GetProcAddress;SDL_GetMouseState=SeriousIOS_OriginalSDL_GetMouseState;SDL_GetKeyboardState=SeriousIOS_OriginalSDL_GetKeyboardState;SDL_GetScancodeFromKey=SeriousIOS_OriginalSDL_GetScancodeFromKey;SDL_GetRelativeMouseState=SeriousIOS_OriginalSDL_GetRelativeMouseState;SDL_PollEvent=SeriousIOS_OriginalSDL_PollEvent;SDL_NumJoysticks=SeriousIOS_OriginalSDL_NumJoysticks;SDL_JoystickNameForIndex=SeriousIOS_OriginalSDL_JoystickNameForIndex;SDL_JoystickOpen=SeriousIOS_OriginalSDL_JoystickOpen;SDL_JoystickClose=SeriousIOS_OriginalSDL_JoystickClose;SDL_JoystickNumAxes=SeriousIOS_OriginalSDL_JoystickNumAxes;SDL_JoystickNumButtons=SeriousIOS_OriginalSDL_JoystickNumButtons;SDL_JoystickNumHats=SeriousIOS_OriginalSDL_JoystickNumHats;SDL_JoystickInstanceID=SeriousIOS_OriginalSDL_JoystickInstanceID;SDL_JoystickGetAxis=SeriousIOS_OriginalSDL_JoystickGetAxis;SDL_JoystickGetButton=SeriousIOS_OriginalSDL_JoystickGetButton;SDL_GetNumAudioDevices=SeriousIOS_OriginalSDL_GetNumAudioDevices;SDL_GetAudioDeviceName=SeriousIOS_OriginalSDL_GetAudioDeviceName;SDL_GetCurrentAudioDriver=SeriousIOS_OriginalSDL_GetCurrentAudioDriver;SDL_OpenAudioDevice=SeriousIOS_OriginalSDL_OpenAudioDevice;SDL_PauseAudioDevice=SeriousIOS_OriginalSDL_PauseAudioDevice;SDL_CloseAudioDevice=SeriousIOS_OriginalSDL_CloseAudioDevice;SDL_LockAudioDevice=SeriousIOS_OriginalSDL_LockAudioDevice;SDL_UnlockAudioDevice=SeriousIOS_OriginalSDL_UnlockAudioDevice")
 endif()
 ''',
         f"{path}: platform root contract",
@@ -57,11 +60,37 @@ endif()
     ${SERIOUSIOS_PLATFORM_ROOT}/SeriousIOSSDLMouse.cpp
     ${SERIOUSIOS_PLATFORM_ROOT}/SeriousIOSSDLInjectedInput.cpp
     ${SERIOUSIOS_PLATFORM_ROOT}/SeriousIOSSDLVirtualController.cpp
+    ${SERIOUSIOS_PLATFORM_ROOT}/SeriousIOSSDLAudioQueue.cpp
     ${SERIOUSIOS_PLATFORM_ROOT}/SeriousIOSSDLGLProcAddress.cpp
     ${SERIOUSIOS_PLATFORM_ROOT}/SeriousIOSOpenGLCompat.cpp
     ${SERIOUSIOS_PLATFORM_ROOT}/SeriousIOSOpenGLImmediateCompat.cpp
     ${SERIOUSIOS_PLATFORM_ROOT}/SeriousIOSOpenGLTextureCompat.cpp
     ${SERIOUSIOS_PLATFORM_ROOT}/SeriousIOSOpenGLExports.cpp
+    ${SERIOUSIOS_PLATFORM_ROOT}/SeriousIOSVorbisRegistry.cpp
+    libogg/src/bitwise.c
+    libogg/src/framing.c
+    libvorbis/lib/analysis.c
+    libvorbis/lib/bitrate.c
+    libvorbis/lib/block.c
+    libvorbis/lib/codebook.c
+    libvorbis/lib/envelope.c
+    libvorbis/lib/floor0.c
+    libvorbis/lib/floor1.c
+    libvorbis/lib/info.c
+    libvorbis/lib/lookup.c
+    libvorbis/lib/lpc.c
+    libvorbis/lib/lsp.c
+    libvorbis/lib/mapping0.c
+    libvorbis/lib/mdct.c
+    libvorbis/lib/psy.c
+    libvorbis/lib/registry.c
+    libvorbis/lib/res0.c
+    libvorbis/lib/sharedbook.c
+    libvorbis/lib/smallft.c
+    libvorbis/lib/synthesis.c
+    libvorbis/lib/vorbisenc.c
+    libvorbis/lib/vorbisfile.c
+    libvorbis/lib/window.c
 ''',
         f"{path}: dynamic loader replacement",
     )
